69c066863b8b5026e5030ada826da3f87a36aa81
tudeverdhangga <tudeverdhangga@gmail.com>
2020-12-07 20:33:16 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/youtubemusic/play/PlayActivity.java
@@ -0,0 +1,16 @@
+package com.example.youtubemusic.play;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.youtubemusic.R;
+
+public class PlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}

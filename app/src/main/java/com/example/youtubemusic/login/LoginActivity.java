dfd9e0e8b6926a38ad31df2119e7e6969048f15a
tudeverdhangga <tudeverdhangga@gmail.com>
2020-12-21 15:41:31 +0800

    Drawable

---
+++ app/src/main/java/com/example/youtubemusic/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.youtubemusic.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.youtubemusic.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
